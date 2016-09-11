33662fc5b850173d97e071d1cb7de64ffe90ba67
Daniel Beeke <daniel@studiofonkel.nl>
2016-09-11 09:13:20 +0200

    more browsers

--- src/test/peer.js
+++ src/test/peer.js
@@ -2,11 +2,9 @@
 
 import Peer from 'src/js/og.core/Peer';
 
-console.log('test')
-
 describe('ES6 Peer', () => {
   it('should return "Do Something" when calling doSomething', () => {
-    expect('b').to.equal('b');
+    expect('c').to.equal('c');
   });
 });
 
