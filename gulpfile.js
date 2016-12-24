3567421e0cb9b8789fcc3482ec5539b44528d7c6
Daniel Beeke <daniel@studiofonkel.nl>
2016-12-24 09:28:50 +0100

    starting with gulp

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var gulp = require('gulp');
+var requireDir = require('require-dir');
+var browserSync = require('browser-sync').create();
+
+process.setMaxListeners(0);
+
+global.paths = {
+    'html': './src/*.html',
+    'js': './src/js/**/*.js'
+};
+
+global.browserSync = browserSync;
+
+requireDir('./gulp', { recurse: false });
+gulp.task('default', ['serve']);
