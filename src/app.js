f1052d26bf10c96d701dd8518fca10ed36f28711
Daniel Beeke <daniel@studiofonkel.nl>
2017-08-12 20:45:07 +0200

    adding a css grid system

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
+import './development.grid.js';
 import Wrapper from 'OpenGroup/core/Wrapper';
 new Wrapper('#app');
---
+++ src/development.grid.js
@@ -0,0 +1,7 @@
+document.addEventListener('keydown', (event) => {
+    if (event.ctrlKey && (event.keyCode === 59 || event.keyCode === 186)) {
+        let newValue = document.body.dataset.grid === 'visible' ? 'invisible' : 'visible';
+        document.body.dataset.grid = newValue;
+        localStorage.setItem('grid', newValue);
+    }
+});
