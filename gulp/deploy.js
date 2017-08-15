7b2d9816e49311c4deb576aa0087f7ac23e740ab
Daniel Beeke <daniel@studiofonkel.nl>
2017-08-15 11:34:35 +0200

    nojekyll seems to work

--- gulp/deploy.js
+++ gulp/deploy.js
@@ -4,9 +4,10 @@ const gulp = require('gulp');
 const ghPages = require('gulp-gh-pages');
 
 gulp.task('deploy', ['build'], function() {
-    return gulp.src('./dist/**/*')
+    return gulp.src(['./dist/**/*', './dist/.nojekyll'])
     .pipe(ghPages({
         remoteUrl: 'git@github.com:opengroup/opengroup.github.io.git',
-        branch: 'master'
+        branch: 'master',
+        force: true
     }));
 });
